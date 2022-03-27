c27c0b22031a19788180e6590ef836c7fee7daf1
Kimame_04 <mendozakyan@gmail.com>
2022-03-27 11:54:40 +0800

    Added links to database for basic querying

--- application.py
+++ application.py
@@ -13,7 +13,7 @@ Session(app)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'admin'
-app.config['MYSQL_DB'] = 'pythonlogin'
+app.config['MYSQL_DB'] = 'vivifica'
 
 mysql = MySQL(app)
 
@@ -33,13 +33,12 @@ def login():
         email = 'todo@todo.com'
 
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
+        cursor.execute('SELECT * FROM account WHERE username = %s', (username,))
         account = cursor.fetchone()
 
         if request.form["submit_btn"] == "login": 
             if account and password == account["password"]:
                 session['loggedin'] = True
-                session['id'] = account['id']
                 session['username'] = account['username']
                 return redirect("/records")
             else:
@@ -55,15 +54,24 @@ def login():
             elif not username or not password or not email:
                 return render_template('login.html', message = "Form incomplete!")
             else:
-                cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, email,))
+                cursor.execute('INSERT INTO account VALUES (%s, %s, %s)', (username, password, email,))
                 mysql.connection.commit()
-                cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
+                cursor.execute('SELECT * FROM account WHERE username = %s', (username,))
                 account = cursor.fetchone()
                 session['loggedin'] = True
-                session['id'] = account['id']
                 session['username'] = account['username']
-                return redirect("/records")
+                return redirect("/register")
     return render_template("login.html")
+
+@app.route("/register", methods = ["GET", "POST"])
+def register():
+    if request.method == 'POST' and 'email' in request.form:
+        email = request.form.get('email')
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('UPDATE account set email = %s where username = %s', (email, session["username"]))
+        mysql.connection.commit()
+        return redirect('/')
+    return render_template("register.html")
 
 @app.route("/logout")
 def logout():
@@ -85,6 +93,54 @@ def records():
 @app.route("/bye")
 def bye():
     return render_template("bye.html")
+
+@app.route("/parts", methods = ["GET", "POST"])
+def parts():
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    if request.method == 'POST' and 'query' in request.form:
+        query = request.form.get('query')
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM part WHERE p_name LIKE %s  or c_name LIKE %s', ['%%' + query + '%%', '%%' + query + '%%'])
+        return render_template("parts.html", parts=cursor.fetchall(), search="true")
+
+    cursor.execute('SELECT * FROM part')
+    return render_template("parts.html", parts=cursor.fetchall())
+
+@app.route("/aircraft", methods = ["GET", "POST"])
+def aircraft():
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    if request.method == 'POST' and 'query' in request.form:
+        query = request.form.get('query')
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM produced_as WHERE aircraft_name LIKE %s  or reg_num LIKE %s', ['%%' + query + '%%', '%%' + query + '%%'])
+        return render_template("aircraft.html", aircrafts=cursor.fetchall(), search="true")
+
+    cursor.execute('SELECT * FROM produced_as')
+    return render_template("aircraft.html", aircrafts=cursor.fetchall())
+
+@app.route("/maintenance", methods = ["GET", "POST"])
+def maintenance():
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    if request.method == 'POST' and 'query' in request.form:
+        query = request.form.get('query')
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM maintenance WHERE c_name LIKE %s  or facility_loc LIKE %s', ['%%' + query + '%%', '%%' + query + '%%'])
+        return render_template("maintenance.html", records=cursor.fetchall(), search="true")
+
+    cursor.execute('SELECT * FROM maintenance')
+    return render_template("maintenance.html", records=cursor.fetchall())
+
+@app.route("/company", methods = ["GET", "POST"])
+def company():
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    if request.method == 'POST' and 'query' in request.form:
+        query = request.form.get('query')
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM company WHERE c_name LIKE %s', ['%%' + query + '%%'])
+        return render_template("company.html", companies=cursor.fetchall(), search="true")
+
+    cursor.execute('SELECT * FROM company')
+    return render_template("company.html", companies=cursor.fetchall())
 
 
 if __name__ == "__main__":
